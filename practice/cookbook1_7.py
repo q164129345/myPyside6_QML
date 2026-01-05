a4915d50e10a119911e7b900c43cfbee5a09847b
wallacezhang <wallace.zhang@fjdynamics.com>
2026-01-05 15:20:43 +0800

    update the code

--- practice/cookbook1_7.py
+++ practice/cookbook1_7.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict
+import json
 
 def ordered_dict():
     d = OrderedDict()
@@ -6,8 +7,8 @@ def ordered_dict():
     d['bar'] = 2
     d['spam'] = 3
     d['grok'] = 4
-    print(d)
-    
+    print(json.dumps(d))
+
     for key in d:
         print(key, d[key])
 
